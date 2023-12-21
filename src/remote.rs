5981cd618e8ca20fe34702f276e2193c86b029ac
pj_zhong <pj_zhong@163.com>
2023-12-21 14:58:12 +0800

    Retrieve remote refs in a separate function

--- src/remote.rs
+++ src/remote.rs
@@ -3,12 +3,18 @@ use crate::data::Ugit;
 impl Ugit {
     pub fn fetch(&mut self, remote_path: String) {
         println!("Will fetch the following refs:");
-        let old_dir = self.change_git_dir(remote_path);
-        for ref_name in self.iter_refs_prefix("refs/heads") {
+        for ref_name in self.get_remote_refs(remote_path, "refs/heads") {
             if self.get_ref_if_not_empty(&ref_name).is_some() {
                 println!("- {ref_name}");
             }
         }
+    }
+
+    fn get_remote_refs(&mut self, remote_path: String, prefix: &str) -> Vec<String> {
+        let old_dir = self.change_git_dir(remote_path);
+        let iter = self.iter_refs_prefix(prefix);
         self.change_git_dir(old_dir);
+
+        iter
     }
 }
